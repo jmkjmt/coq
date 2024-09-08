7cce90c55aa6ca15ddff885a016ad144f804562b
jmk_jmt <zlsdalsrb@korea.ac.kr>
2024-09-08 18:57:16 +0900

    update

--- sigma.v
+++ sigma.v
@@ -1,21 +1,60 @@
 Require Import Arith.
 Require Import Program.
+Require Import Lia.
+
+(* f : Z -> Z : a와 b가 정수형(BigInt)을 사용하므로 Z로 설정 *)
+(* 종료성을 위해 measure로 b - a를 사용 *)
+Theorem neq :forall a:nat, S a =? a = false.
+Proof.
+  intros.
+  induction a.
+  reflexivity.
+  auto.
+  Qed.
+Theorem lt: forall a:nat, S a <? a = false.
+Proof.
+  intros.
+  induction a.
+  reflexivity.
+  auto.
+  Qed.
+Theorem lt2 : forall a:nat, S a - a < 2.
+Proof.
+  intros.
+  induction a.
+  auto.
+  auto.
+  Qed.
+Program Fixpoint solution_1 (f: nat -> nat) (a: nat) (b: nat) {measure (if Nat.eqb a b then 2 else if Nat.ltb a b then 2 + (b - a) else (a - b))}: nat :=
+  if Nat.ltb b a then 0
+  else if Nat.eqb a b then f a
+  else f a + solution_1 f (a + 1) b.
+  
+  Next Obligation.
+  SearchRewrite (_ + _).
+  rewrite Nat.add_1_r.
+  rewrite Nat.sub_succ_r.
+  case (b=?a) eqn:Ha.
+  rewrite Nat.eqb_eq in Ha.
+  rewrite Ha.
+  rewrite neq.
+  Search (S _ < _).
+  rewrite lt.
+  rewrite Nat.ltb_irrefl.
+  SearchRewrite (_ =? _).
+  rewrite Nat.eqb_refl.
+  apply lt2.
+  
+
+  
+
+  
+  
+ 
 
 
-(* Coq에서 BigInt는 nat로 표현 *)
-Program Fixpoint solution_1 (f: nat -> nat) (a: nat) (b: nat) {measure (b - a)} : nat :=
-  if Nat.ltb b a then 0 (* a > b *)
-  else if Nat.eqb a b then f a (* a = b *)
-  else f a + solution_1 f (S a) b.
 
-(* 종료성 증명 *)
-Next Obligation.
-intuition.
-
-Qed.
-
-
-Program Fixpoint solution_2 (f: nat -> nat) (a: nat) (b : nat) {measure (b - a)} : nat :=
+Program Fixpoint solution_2 (f: nat -> nat) (a: nat) (b : nat) : nat :=
     if (Nat.ltb b a) then 0 
     else (f b) + solution_2 f a (b-1)
 .
