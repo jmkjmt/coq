957496ab6a7bb45d6f950b95954634f1123269cf
jmk_jmt <mingyu_jo@korea.ac.kr>
2024-09-10 18:43:39 +0900

    update

--- iter.v
+++ iter.v
@@ -1,8 +1,57 @@
 Require Import ZArith.
+Require Import Arith.
+Require Import Program.
 Require Import Lia.
-Open Scope Z_scope.
+Require Import Coq.Arith.Wf_nat.
 
-Fixpoint solution (n:Z) (f:Z->Z) : Z->Z :=
-if (Z.ltb n  0) then f
+
+Definition foo (n: nat) (f: Z -> Z) : Z -> Z.
+
+(* Prove termination using well-founded recursion *)
+Proof.
+  apply (well_founded_induction lt_wf (fun n => (Z -> Z))).
+
+  intros n1 IH.
+
+  (* Now define the function body using the induction hypothesis *)
+  destruct (Nat.eqb n 0) eqn:H.
+  - (* Base case: when n = 0 *)
+    exact (fun x => x).
+  - (* Recursive case: when n > 0 *)
+    exact (fun x => f (IH (n - 1) (Nat.sub_lt n 0) f x)).
+Defined.
+
+Program Fixpoint sol (n: nat) (f: Z -> Z) (x: Z) {measure (n)}: Z :=
+if Nat.eqb n 0 then x
+else f (sol (n - 1) f x).
+
+Next Obligation.
+Proof.
+
+
+
+induction n.
+simpl.
+
+
+
+
+
+(* fun (x: Z) => f ((sol (n - 1) f) x). *)
+
+
+Fixpoint asdfasdfadfasdfas (n: nat) (f: Z -> Z): (Z -> Z) :=
+if (Nat.ltb n 0) then f
+else if (Nat.eqb n 0) then fun x:Z => x
+else fun (x:Z) => f ((asdfasdfadfasdfas (n-1) f) x).
+
+
+Program Fixpoint solution (n: nat) (f: Z -> Z) {measure (n)}: (Z -> Z) :=
+if (Nat.ltb n 0) then f
+else if (Nat.eqb n 0) then fun x:Z => x
+else fun (x:Z) => f ((solution (n-1) f) x).
+
+Fixpoint solution2 (n:Z) (f:Z->Z) : Z->Z :=
+if (Z.ltb n 0) then f
 else if (Z.eqb n 0) then fun x:Z => x
-else fun (x:Z) => f (solution (n-1) f x).
+else fun (x:Z) => f (solution2 (n-1) f x).
--- uniq.v
+++ uniq.v
@@ -7,7 +7,7 @@ Import ListNotations.
 Fixpoint remove_elem (A:Type) (e: A) (lst:list A) : list A :=
     match lst with
     | [] => []
-    | hd::tl => if e = hd then remove_elem e tl else hd::(remove_elem e tl)
+    | hd::tl => if e =? hd then remove_elem e tl else hd::(remove_elem e tl)
     end.
 
 Fixpoint solution (A:Type) (lst:list A) : list A :=
