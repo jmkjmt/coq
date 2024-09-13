ec96e18a25832c4dad45b3176db6b6515199e461
jmk_jmt <mingyu_jo@korea.ac.kr>
2024-09-13 17:14:33 +0900

    update

--- filter.v
+++ filter.v
@@ -12,12 +12,27 @@ Fixpoint solution (pred:Z -> bool) (lst:list Z) : list Z :=
     end.
 
 Definition filter_sub (pred:Z -> bool) (lst: list Z) : list Z :=
-    let fix aux (i:nat) (l :list Z) : list Z :=
+    let fix aux (i:Z) (l :list Z) : list Z :=
     match l with
     | [] => []
-    | hd::tl => if pred hd then hd:: aux (S i) tl else aux (S i) tl
+    | hd::tl => if pred hd then hd:: aux (i + 1) tl else aux (i + 1) tl
     end in
-    aux (1:nat) lst.
+    aux (1:Z) lst.
+
+
+Definition filter_sub2 (pred: Z -> bool) (lst: list Z) := 
+	let fix loop (i: list Z) (o: list Z) :=
+		match i with
+		|	[] => o
+		|	h :: t => loop t (if (pred h) then(h :: o) else loop t o)
+    end in
+	let fix reverse (i: list Z) (o: list Z) :=
+		match i with
+		|	[] => o
+		|	h :: t => reverse t (h :: o) end in
+	reverse (loop lst []) [].
+
+
 
 Lemma cons_injective : forall  (x : Z) (l1 l2 : list Z),
   x :: l1 = x :: l2 <-> l1 = l2.
@@ -35,14 +50,14 @@ Theorem eq : forall pred lst, solution pred lst = filter_sub pred lst.
 Proof.
     intros.
     assert (lemma: forall lst, forall i j, 
-    (fix aux (i : nat) (l : list Z) {struct l} : list Z := match l with
+    (fix aux (i : Z) (l : list Z) {struct l} : list Z := match l with
     | [] => []
-    | hd :: tl => if pred hd then hd :: aux (S i) tl else aux (S i) tl
-    end) (i:nat) lst = 
-    (fix aux (i : nat) (l : list Z) {struct l} : list Z := match l with
+    | hd :: tl => if pred hd then hd :: aux (i + 1) tl else aux (i + 1) tl
+    end) (i:Z) lst = 
+    (fix aux (i : Z) (l : list Z) {struct l} : list Z := match l with
     | [] => []
-    | hd :: tl => if pred hd then hd :: aux (S i) tl else aux (S i) tl
-    end) (j:nat) lst).
+    | hd :: tl => if pred hd then hd :: aux (i + 1) tl else aux (i + 1) tl
+    end) (j:Z) lst).
     induction lst0.
     reflexivity.
     case (pred a).
@@ -71,3 +86,16 @@ Proof.
     unfold filter_sub.
     apply lemma.
     Qed.
+
+Theorem eq2: forall pred lst, solution pred lst = filter_sub2 pred lst.
+Proof.
+  intros.
+  induction lst.
+  reflexivity.
+  simpl.
+  case (pred a) eqn: Ha.
+  unfold filter_sub2.
+  rewrite Ha.
+  unfold filter_sub2 in IHlst.
+  rewrite IHlst.
+  
