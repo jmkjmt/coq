1444d3c359d3d90572ae82fc7e3401531b4157f3
jmk_jmt <mingyu_jo@korea.ac.kr>
2024-11-27 19:32:34 +0900

    update

--- test.v
+++ test.v
@@ -1,104 +1,38 @@
-Require Import Prelude.
-
+Require Import Arith ZArith List Bool String.
+Definition int : Type := Z.
 Inductive natural : Type :=
- ZERO : natural
-| SUCC : natural -> natural.
-
+  | ZERO : natural
+  | SUCC : natural -> natural.
 Fixpoint natadd n1 n2 :=
   match n1 with
-   ZERO => n2
+  | ZERO => n2
   | SUCC n => SUCC (natadd n n2)
   end.
-Fixpoint natadd_ref n1 n2 :=
-match n1 with
-| ZERO => n2
-| SUCC ZERO => SUCC n2
-| SUCC n => SUCC (natadd_ref n n2)
-end.
-Fixpoint natadd2 n1 n2 :=
+Fixpoint natadd_sub n1 n2 :=
   match n1 with
-   ZERO => n2
-  | SUCC n =>  natadd2 n (SUCC n2)
+  | ZERO => n2
+  | SUCC n => natadd_sub n (SUCC n2)
   end.
-Fixpoint natadd3 n1 n2 :=
-match n2 with
-| ZERO => n1
-| SUCC n => SUCC (natadd3 n1 n)
-end.
-Theorem equal : forall arg0 arg1, natadd arg0 arg1 = natadd_ref arg0 arg1.
-Proof.
-  induction arg0.
-  simpl.
-  reflexivity.
-  simpl.
-  intros.
-  case arg0 eqn:H.
-  simpl.
-  reflexivity.
-  simpl in *.
-  simpl in IHarg0.
-  rewrite IHarg0.
-  reflexivity.
-  Qed.
-
-Theorem test: forall arg0 arg1, natadd arg0 arg1 = natadd_ref arg0 arg1.
-Proof.
-  (induction arg0).
-(simpl in *).
-reflexivity.
-(intros arg1).
-(simpl in *).
-(destruct (arg0) eqn:arg0case).
-(simpl in *).
-reflexivity.
-(rewrite <- IHarg0 in *).
-(simpl in *).
-reflexivity.
-Qed.
-
-
-
-Theorem sub4:forall arg0 arg1, natadd arg0 arg1 = natadd3 arg0 arg1.
-Proof.
-  induction arg0.
-  simpl.
-  intros.
-  assert (lemma1: arg1 = natadd3 ZERO arg1).
-    generalize dependent arg1.
-    induction arg1.
-    simpl.
-    reflexivity.
-    simpl.
-    rewrite <- IHarg1.
-    reflexivity.
-  rewrite <- lemma1.
-  reflexivity.
-  simpl.
-  intros arg1.
-  rewrite IHarg0.
-  assert (lemma2: forall arg0 arg1, SUCC (natadd3 arg0 arg1) = natadd3 (SUCC arg0) arg1).
-  {
-    intros.
-    generalize dependent arg2.
-    induction arg3.
-    simpl.
-    reflexivity.
-    simpl.
-    intros arg2.
-    rewrite IHarg3.
-    reflexivity.
-  }
-  rewrite lemma2.
-  reflexivity.
-  Qed.
-
-  Theorem test1:forall arg0 arg1, natadd arg0 arg1 = natadd3 arg0 arg1.
+Theorem equiv : forall arg0 arg1, natadd arg0 arg1 = natadd_sub arg0 arg1.
 Proof.
 (induction arg0).
 (simpl in *).
+reflexivity.
+(simpl in *).
 (intros arg1).
 (simpl in *).
-
-
+(rewrite <- IHarg0 in *).
+(simpl in *).
+(assert (forall arg0_, SUCC (natadd arg0_ arg1) = natadd arg0_ (SUCC arg1))).
+(induction arg0_).
+(simpl in *).
+reflexivity.
+(simpl in *).
+(rewrite <- IHarg0_ in *).
+(simpl in *).
+reflexivity.
+(simpl in *).
+(rewrite <- H in *).
+(simpl in *).
+reflexivity.
 Qed.
-
