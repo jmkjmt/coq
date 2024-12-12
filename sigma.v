3fbe77d4c2d11e51580333b67f42b215dd5f450a
jmk_jmt <mingyu_jo@korea.ac.kr>
2024-12-12 13:31:00 +0900

    update

--- sigma.v
+++ sigma.v
@@ -1,16 +1,15 @@
 Require Import Program Arith ZArith Lia.
 
   
- Program Fixpoint solution_1 (f: Z -> Z) (a b: Z) {measure (Z.to_nat (b-a))}: Z :=
+ (* Program Fixpoint solution_1 (f: Z -> Z) (a b: Z) {measure (Z.to_nat (b-a + 1))}: Z :=
   if Z.ltb b a then 0
   else if Z.eqb a b then f a
   else f a + solution_1 f (a + 1) b.
 
   Next Obligation.
-  intuition.
-  
+  Proof. *)
+    
 
-  
 
 Program Fixpoint solution_2 (f: nat -> nat) (a b : nat) {measure (b-a)} : nat :=
     if (Nat.ltb b a) then 0 
@@ -18,8 +17,16 @@ Program Fixpoint solution_2 (f: nat -> nat) (a b : nat) {measure (b-a)} : nat :=
   
 
 Program Fixpoint solution_2 (f: nat -> nat) (a: nat) (b : nat) : nat :=
-    if (Nat.ltb b a) then 0 
-    else (f b) + solution_2 f a (b-1)
+    ,match b - a with
+    | 0 => f b
+    | _ => f b + solution_2 f a (b-1)
+    end.
+Next Obligation.
+Proof.
+    specialize (solution_2 f a (b-1)).
+    apply solution_2.
+
+    
 .
 
 Program Fixpoint aux (a:nat) (b:nat) (f:nat->nat) (acc:nat) : nat :=
